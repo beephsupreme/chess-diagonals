18a89b97939bcdd5c4eeac4b4bb7b751abd07f93
Michael Rowsey <mike@rowsey.org>
2023-07-15 19:54:43 -0700

    soln 3

--- js/chessboard.js
+++ js/chessboard.js
@@ -3,7 +3,7 @@ export default {
 	highlight
 };
 
-var tiles;
+var tiles = [];
 
 // ****************************
 
@@ -11,19 +11,20 @@ function draw(boardEl) {
 	// TODO: draw the chessboard, 8 rows (divs)
 	// of 8 tiles (divs) each, inserting all DOM
 	// elements into `boardEl` div
-	var origBoardEl = boardEl;
 	
 	for (let i = 0; i < 8; i++) {
 		let rowEl = document.createElement("div");
+		let rowTiles = [];
+		tiles.push(rowTiles);
 		for (let j = 0; j < 8; j++) {
 			let tileEl = document.createElement("div");
 			tileEl.dataset.row = i;
 			tileEl.dataset.col = j;
 			rowEl.appendChild(tileEl);
+			rowTiles.push(tileEl);
 		}
 		boardEl.appendChild(rowEl);
 	}
-	tiles = origBoardEl.querySelectorAll("div > div");
 }
 
 function highlight(tileEl) {
@@ -34,46 +35,38 @@ function highlight(tileEl) {
 	
 	
 	// clear all currently highlighted tiles
-	for (let el of tiles) {
-		el.classList.remove("highlighted");
+	for (let row of tiles) {
+		for (let el of row) {
+			el.classList.remove("highlighted");
+		}
 	}
 
 	if (tileEl) {
-		let rowEl = tileEl.parentNode;
-		let boardEl = rowEl.parentNode;
-		let tileRowIdx = [ ...boardEl.childNodes ].indexOf(rowEl); 
-		let tileColIdx = [ ...rowEl.childNodes ].indexOf(tileEl);
+		let tileRowIdx = Number(tileEl.dataset.row); 
+		let tileColIdx = Number(tileEl.dataset.col); 
 
 		// highlight in up-left direction
 		for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j >=0; i--, j--) {
-			let el = findTile(i, j);
+			let el = tiles[i][j]; 
 			el.classList.add("highlighted");
 		}
 
 		// highlight in up-right direction
 		for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j < 8; i--, j++) {
-			let el = findTile(i, j);
-			el.classList.add("highlighted");
+			let el = tiles[i][j]; 
+ 			el.classList.add("highlighted");
 		}
 
 		// highlight in down-left direction
 		for (let i = tileRowIdx, j = tileColIdx; i < 8 && j >=0; i++, j--) {
-			let el = findTile(i, j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
 		}
 
 		// highlight in down-right direction
 		for (let i = tileRowIdx, j = tileColIdx; i < 8 && j < 8; i++, j++) {
-			let el = findTile(i, j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
-		}
-	}
-
-	function findTile(row, col) {
-		for (let el of tiles) {
-			if (el.dataset.row == row && el.dataset.col == col) {
-				return el;
-			}
 		}
 	}
 }
